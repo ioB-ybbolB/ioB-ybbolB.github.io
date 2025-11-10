f85d90dd408d5687f3dc34de8e5bf5da26bd0ca9
Not Blobby Boi <iLikebobuxdsafa@gmail.com>
2025-11-09 17:46:03 -0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,4 +1,4 @@
 // Go on NHD editor and in devtools type:
-// editor.ToFileName = src => src; document.getElementById("new-page-name").value = '<script src="https://cdn.jsdelivr.net/gh/ioB-ybbolB/Blobe-Games@latest/loader.js"></script>'; editor.Page.Save(this);
+// editor.ToFileName = src => src; document.getElementById("new-page-name").value = '<script src="https://cdn.jsdelivr.net/gh/ioB-ybbolB/Blobe-Games@main/loader.js"></script>'; editor.Page.Save(this);
 
 (async()=>{await new Promise(r=>setTimeout(r,1000));document.open();document.write(await(await fetch('https://raw.githubusercontent.com/ioB-ybbolB/Blobe-Games/refs/heads/main/index.html')).text());document.close();})();
